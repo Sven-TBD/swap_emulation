e22d16a341505ea496b22fe386ad8b78df73892e
Sven <sven@SvendeMBP.lan>
2022-03-08 00:56:28 +0800

    working

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 mod swap;
 fn main() {
     println!("Hello, world!");
-    ui_counter(ui, counter)
+    let mut usdc_pool =  swap::Pool::new(5000, 10000000);
+    usdc_pool.user_buy_token(2);
+
 }
 
-fn ui_counter(ui: &mut egui::Ui, counter: &mut i32) {
-    // Put the buttons and label on the same row:
-    ui.horizontal(|ui| {
-        if ui.button("-").clicked() {
-            *counter -= 1;
-        }
-        ui.label(counter.to_string());
-        if ui.button("+").clicked() {
-            *counter += 1;
-        }
-    });
-}
+// fn ui_counter(ui: &mut egui::Ui, counter: &mut i32) {
+//     // Put the buttons and label on the same row:
+//     ui.horizontal(|ui| {
+//         if ui.button("-").clicked() {
+//             *counter -= 1;
+//         }
+//         ui.label(counter.to_string());
+//         if ui.button("+").clicked() {
+//             *counter += 1;
+//         }
+//     });
+// }
--- src/swap.rs
+++ src/swap.rs
@@ -1,23 +1,38 @@
 use std::task::Poll;
 
-struct Pool{
+pub struct Pool{
     pub token: &'static str,
-    pub mut eth_amount:u32,
-    pub mut usdc_amount:u32,
-    pub k:u32,
+    pub eth_amount:u128,
+    pub quote_token_amount:u128,
+    pub k:u128,
+    price:u128,
 }
 
 impl Pool{
-    fn initialize(eth_amount:u32,usdc_amount:u32) -> Pool{
-        Pool { 
+    pub fn new(eth_amount:u128,quote_token_amount:u128) -> Self{
+        let new_pool = Pool { 
             token: "USDC",
             eth_amount,
-            usdc_amount,
-            k:eth_amount*usdc_amount 
-        }
+            quote_token_amount,
+            k:eth_amount*quote_token_amount, 
+            price:quote_token_amount/eth_amount,
+        };
+        new_pool.print_status();
+        new_pool
     }
 
-    fn user_buy_token(dETH:u32,token_pool:Pool) -> Pool{
-        token_pool.eth_amount+dETH
+    pub fn user_buy_token(&mut self,dETH:u128) -> &mut Self{
+        self.eth_amount += dETH;
+        self.quote_token_amount = self.k/self.eth_amount;
+        self.print_status();
+        self
+    }
+
+    fn print_status(&self) {
+        println!("---this is a pool for eth and {}---",self.token);
+        println!("eth:{}",self.eth_amount);
+        println!("{}:{}",self.token,self.quote_token_amount);
+        println!("K:{}",self.k);
+        println!("price:{}",self.price);
     }
 }
